656d24e13e4ac9fc861491b966762baa7d3a958e
oscargonzalezquiros <61214791+oscargonzalezquiros@users.noreply.github.com>
2020-06-30 21:04:45 -0600

    Adelanto Proyecto

---
+++ src/app/models/tarea.ts
@@ -0,0 +1,7 @@
+export class Tarea {
+public id: string;
+public descripcion: string;
+public fecha: Date;
+public usuario: string;
+public estado: boolean;
+}
--- src/app/service/principal.service.ts
+++ src/app/service/principal.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
+import { Tarea } from '../models/tarea';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PrincipalService {
-
+  public tareas: Tarea[];
   constructor() { }
 }
