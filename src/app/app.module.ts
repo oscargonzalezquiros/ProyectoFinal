87d4f9ec75f9dac035aedb8e2e50cbbeadd33d0f
oscargonzalezquiros <61214791+oscargonzalezquiros@users.noreply.github.com>
2020-06-30 19:20:24 -0600

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { ItemComponent } from './components/item/item.component';
+import { ListaComponent } from './components/lista/lista.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FormularioComponent,
+    ItemComponent,
+    ListaComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
